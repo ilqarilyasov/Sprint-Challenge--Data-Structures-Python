d72b2feab44871a8b3ede45934763b12673caba5
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-21 13:06:17 -0500

    A solution with while

--- names/names.py
+++ names/names.py
@@ -1,25 +1,34 @@
 import time
-import os
 
 start_time = time.time()
-dirpath = os.getcwd()
-file1 = f'{dirpath}/names/names_1.txt'
-file2 = f'{dirpath}/names/names_2.txt'
 
-f = open('names_1.txt', 'r')
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+names_1 = ["a", "b", "c", "d"]
 
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+names_2 = ["e", "f", "g", "h", "i", "j"]
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+# for i in range(0, len(names_1)):
+#     j = 0
+#     while j < (len(names_2) - 1):
+#         if names_1[i] == names_2[j]:
+#             duplicates.append(names_1[i])
+#         elif names_1[i] == names_2[j+1]:
+#             duplicates.append(names_1[i])
+#         j += 2
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
